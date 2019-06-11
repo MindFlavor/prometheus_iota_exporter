cf5bcd4e923065db71156b650713bc1d2df2fefd
Francesco Cogno <francesco.cogno@outlook.com>
2019-06-11 13:37:52 +0200

    Cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ extern crate serde_derive;
 #[macro_use]
 extern crate failure;
 use clap::{crate_authors, crate_name, crate_version, Arg};
-use futures::future::{done, ok, Future};
+use futures::future::{done, ok, Either, Future};
 use futures::stream::Stream;
-use http::StatusCode;
-use hyper::service::service_fn;
 use hyper::Client;
-use hyper::{Body, Request, Response, Server};
-use log::{debug, error, info, trace, warn};
+use hyper::{Body, Request, Response};
+use log::{debug, info, trace};
 use std::env;
 mod exporter_error;
 use crate::exporter_error::ExporterError;
@@ -20,52 +18,10 @@ mod get_neighbors;
 use get_neighbors::Neighbors;
 mod options;
 use crate::options::Options;
+use serde::de::DeserializeOwned;
 mod render_to_prometheus;
 use prometheus_exporter_base::render_prometheus;
 use render_to_prometheus::RenderToPrometheus;
-
-fn handle_request(
-    req: Request<Body>,
-    options: &Options,
-) -> impl Future<Item = Response<Body>, Error = failure::Error> {
-    debug!("{:?}", req);
-
-    let r = if options.exclude_neighbors {
-        perform_request_simple(req, options)
-    } else {
-        perform_request_with_neighbors(req, options)
-    };
-
-    r.then(|res| match res {
-        Ok(body) => ok(body),
-        Err(inner_error) => match inner_error {
-            ExporterError::UnsupportedPathError { path: ref _path } => {
-                warn!("{:?}", inner_error);
-                let r = Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(hyper::Body::empty())
-                    .unwrap();
-                ok(r)
-            }
-            ExporterError::UnsupportedMethodError { verb: ref _verb } => {
-                warn!("{:?}", inner_error);
-                let r = Response::builder()
-                    .status(StatusCode::METHOD_NOT_ALLOWED)
-                    .body(hyper::Body::empty())
-                    .unwrap();
-                ok(r)
-            }
-            _ => {
-                error!("{:?}", inner_error);
-                let r = Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(hyper::Body::empty())
-                    .unwrap();
-                ok(r)
-            }
-        },
-    })
-}
 
 #[inline]
 fn extract_body(
@@ -80,153 +36,41 @@ fn extract_body(
     }))
 }
 
-fn perform_request_with_neighbors(
-    req: Request<Body>,
-    options: &Options,
-) -> Box<Future<Item = Response<Body>, Error = ExporterError> + Send> {
-    debug!("perform_request_with_neighbors");
-    Box::new(
-        done(check_params_and_prepare_requests(req, options))
-            .from_err()
-            .and_then(|(future_node_info, future_exclude_neighbors)| {
-                extract_body(future_node_info)
-                    .join(extract_body(future_exclude_neighbors.unwrap()))
-                    .from_err()
-                    .and_then(|(node_info_str, neighbors_info_str)| {
-                        debug!("node_info_str == {:?}", node_info_str);
-                        debug!("neighbors_info_str == {:?}", neighbors_info_str);
-
-                        done(serde_json::from_str(&node_info_str))
-                            .from_err()
-                            .and_then(move |node_info: NodeInfo| {
-                                debug!("{:?}", node_info);
-
-                                done(serde_json::from_str(&neighbors_info_str))
-                                    .from_err()
-                                    .and_then(move |get_neighbors: Neighbors| {
-                                        debug!("{:?}", get_neighbors);
-                                        let response = format!(
-                                            "{}\n{}",
-                                            node_info.render(),
-                                            get_neighbors.render()
-                                        );
-                                        ok(Response::new(Body::from(response)))
-                                    })
-                            })
-                    })
-            }),
-    )
-}
-
-fn perform_request_simple(
-    req: Request<Body>,
-    options: &Options,
-) -> Box<Future<Item = Response<Body>, Error = ExporterError> + Send> {
-    debug!("perform_request_simple");
-    Box::new(
-        done(check_params_and_prepare_requests(req, options))
-            .from_err()
-            .and_then(|(future_node_info, _)| {
-                extract_body(future_node_info)
-                    .from_err()
-                    .and_then(|node_info_str| {
-                        debug!("node_info_str == {:?}", node_info_str);
-
-                        done(serde_json::from_str(&node_info_str))
-                            .from_err()
-                            .and_then(|node_info: NodeInfo| {
-                                debug!("{:?}", node_info);
-                                ok(Response::new(Body::from(node_info.render())))
-                            })
-                    })
-            }),
-    )
-}
-
-fn check_params_and_prepare_requests(
-    req: Request<Body>,
-    options: &Options,
-) -> Result<
-    (
-        hyper::client::ResponseFuture,
-        Option<hyper::client::ResponseFuture>,
-    ),
-    ExporterError,
-> {
-    if req.method() != hyper::Method::GET {
-        warn!("Only GET is supported, received {}", req.method());
-        Err(ExporterError::UnsupportedMethodError {
-            verb: req.method().to_string(),
-        }
-        .into())
-    } else if req.uri() != "/metrics" {
-        Err(ExporterError::UnsupportedPathError {
-            path: req.uri().to_string(),
-        }
-        .into())
-    } else {
-        //let uri: hyper::Uri = "http://dns.mindflavor.it:14267".parse().unwrap();
-        let cli = Client::new();
-
-        let mut request = hyper::Request::builder();
-        request
-            .method("PUT")
-            .uri(options.iri_uri.clone())
-            .header("X-IOTA-API-Version", "1")
-            .header("Content-Type", "application/json");
-        let request_node_info = request.body(Body::from("{\"command\": \"getNodeInfo\"}"))?;
-
-        if options.exclude_neighbors {
-            Ok((cli.request(request_node_info), None))
-        } else {
-            let mut request = hyper::Request::builder();
-            request
-                .method("PUT")
-                .uri(options.iri_uri.clone())
-                .header("X-IOTA-API-Version", "1")
-                .header("Content-Type", "application/json");
-            let request_neighbors = request.body(Body::from("{\"command\": \"getNeighbors\"}"))?; //O
-
-            Ok((
-                cli.request(request_node_info),
-                Some(cli.request(request_neighbors)),
-            ))
-        }
-    }
-}
-
-fn perform_request_new(
-    req: Request<Body>,
+fn perform_request(
+    _req: Request<Body>,
     options: &Arc<Options>,
 ) -> impl Future<Item = Response<Body>, Error = failure::Error> {
     trace!("perform_request");
 
-    let fut_get_node_info = create_iri_future("getNodeInfo", options)
-        .and_then(|node_info_str| done(serde_json::from_str(&node_info_str)).from_err())
-        .and_then(|node_info: NodeInfo| {
-            debug!("{:?}", node_info);
-            ok(node_info)
-        });
+    let fut_get_node_info = create_iri_future::<NodeInfo>("getNodeInfo", options);
 
-    let fut_get_neighbors = create_iri_future("getNeighbors", options)
-        .and_then(|get_neigh_str| done(serde_json::from_str(&get_neigh_str)).from_err())
-        .and_then(|get_neighbors: Neighbors| {
-            debug!("{:?}", get_neighbors);
-            ok(get_neighbors)
-        });
+    if options.exclude_neighbors {
+        Either::A(
+            fut_get_node_info
+                .and_then(|node_info| ok(Response::new(Body::from(node_info.render())))),
+        )
+    } else {
+        Either::B({
+            let fut_get_neighbors = create_iri_future::<Neighbors>("getNeighbors", options);
 
-    fut_get_node_info
-        .join(fut_get_neighbors)
-        .and_then(|(node_info, get_neighbors)| {
-            let response = format!("{}\n{}", node_info.render(), get_neighbors.render());
-            ok(Response::new(Body::from(response)))
+            // we join the two futures so they will run concurrently
+            fut_get_node_info
+                .join(fut_get_neighbors)
+                .and_then(|(node_info, get_neighbors)| {
+                    let response = format!("{}\n{}", node_info.render(), get_neighbors.render());
+                    ok(Response::new(Body::from(response)))
+                })
         })
+    }
 }
 
-fn create_iri_future(
+fn create_iri_future<T>(
     command: &str,
     options: &Arc<Options>,
-) -> impl Future<Item = String, Error = failure::Error> {
+) -> impl Future<Item = T, Error = failure::Error>
+where
+    T: DeserializeOwned + std::fmt::Debug,
+{
     let cli = Client::new();
     let mut request = hyper::Request::builder();
 
@@ -241,8 +85,13 @@ fn create_iri_future(
     .from_err()
     .and_then(move |request| extract_body(cli.request(request)).from_err())
     .and_then(|text: String| {
-        debug!("{:?}", text);
+        debug!("received_text == {:?}", text);
         ok(text)
+    })
+    .and_then(|text| done(serde_json::from_str(&text)).from_err())
+    .and_then(|t: T| {
+        debug!("received_object == {:?}", t);
+        ok(t)
     })
 }
 
@@ -302,7 +151,7 @@ fn main() {
     info!("starting exporter on {}", addr);
 
     render_prometheus(&addr, options, |request, options| {
-        Box::new(perform_request_new(request, options))
+        Box::new(perform_request(request, options))
     });
 
     //let new_svc = move || {
